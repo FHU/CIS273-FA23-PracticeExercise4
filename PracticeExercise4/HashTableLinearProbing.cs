ec87b2d0ec4115fdb518b4e661ad8613b0ab0961
Kenan Casey <kenancasey@gmail.com>
2023-12-05 08:15:10 -0600

    initial commit

---
+++ PracticeExercise4/HashTableLinearProbing.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace PracticeExercise4
+{
+	public class HashTableLinearProbing<K,V>: IHashTable<K,V>
+	{
+		private Bucket<K, V>[] buckets;
+		private int initialCapacity = 16;
+
+		public HashTableLinearProbing()
+		{
+			buckets = new Bucket<K, V>[initialCapacity];
+
+			for(int i=0; i< initialCapacity; i++)
+			{
+				buckets[i] = new Bucket<K, V>();
+			}
+		}
+
+        private int count;
+        public int Count => count;
+
+        public double LoadFactor => throw new NotImplementedException();
+
+        public bool Add(K key, V value)
+        {
+            // get hash
+
+            // find the bucket index for hash
+
+            // while collision, go to the next bucket
+
+            // add key, value, update count
+            // OR update
+
+
+            // return true if update, false if add
+        }
+
+        public bool ContainsKey(K key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsValue(V value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public V Get(K key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<K> GetKeys()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<V> GetValues()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(K key)
+        {
+            throw new NotImplementedException();
+        }
+
+        private int Hash(K key)
+        {
+            int hash = key.GetHashCode();
+
+            return hash < 0 ? -hash : hash;
+        }
+
+    }
+}
+
